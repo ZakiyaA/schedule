55aa0abab5acc517fddb9f2a2fd6307f96da2e91
Zakiya <zakiyaalfughi@gmail.com>
2022-01-23 16:21:03 -0500

    Add custom hook to switch between moods

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  // const EMPTY = "EMPTY";
+  // const SHOW = "SHOW";
+  
   return(
     <article className="appointment">
       <Header/>
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,54 @@
-import {React, useState} from "react";
+// import {React, useState} from "react";
+
+
+  
+// export default function useVisualMode(initial) {
+//   const [history, setHistory] = useState(initial);
+//   // const [mode, setMode] = useState(initial);
+//   function transition(nextMode) {
+//     const nextHistory = [...history];
+//     nextHistory.push(nextMode);
+//     setHistory(nextHistory);
+//   };
+//   const back = function(){
+//     const nextHistory = [...history];
+//     nextHistory.pop();
+//     setHistory(nextHistory);
+//   };
+//   const mode = history.slice(-1)[0];
+//   return { mode, transition, back, history};
+// }
+
+
+
+
+
+
+import { useState } from 'react'
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
- 
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+  function transition(nextMode, replace = false) {
+    setMode(nextMode)
+    if (replace) {
+      setHistory(prev => [prev[0]])
+    }
+    setHistory(prev => [...prev, nextMode])
+  }
+  function back() {
+    history.pop()
+    if (history.length) {
+      const prevMode = history[history.length - 1]
+      setMode(prevMode)
+    }
+  }
+  return { mode, transition, back }
+}
 
-  return { mode};
-};
+
+// const [setSome, setSomeState] = useState({thing: 'loding', count: 1});
+// setSomeState(prev => ({...prev, count: prev.count + 1}));
 
 
 
